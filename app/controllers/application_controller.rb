f258629aaaebaed48893e88c2578d8bb7d90ecae
vagrant <vagrant@localhost.localdomain>
2020-03-10 15:05:31 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+
+	before_action :authenticate_user!
+	#before_actionメソッドは、このコントローラが動作する前に実行される
+	#:authenticate_user!はログイン認証されていなければ、ログイン画面へリダイレクトする、という機能を実装できる
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	#devise利用の機能（ユーザ登録、ログイン認証など）が使われる場合、その前にconfigure_permitted_parametersが実行されます。
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	end
+	#configure_permitted_parametersでは、devise_parameter_sanitizer.permitでnameのデータ操作を許可するアクションメソッドが指定されています。今回のケースでは、ユーザ登録（sign_up）の際に、ユーザ名（name）のデータ操作が行われます。
+
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'a5eab92e2810b107909ca3bf7c2ca50bf2c9063026d7db69fd0d17074cac19a6ddc96031aa83ef2f6e22439c0c55870608ce5324f1b7067ddcd4fef3a2456330'
+
